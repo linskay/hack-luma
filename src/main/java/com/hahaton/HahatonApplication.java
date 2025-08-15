dbe1c1af0aad82d8a1bfe7312d8558ab45def0c0
Lins <alina.cheremiskina@gmail.com>
2025-08-15 22:10:02 +0300

    save

---
+++ src/main/java/com/hahaton/HahatonApplication.java
@@ -0,0 +1,13 @@
+package com.hahaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HahatonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HahatonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hahaton/HahatonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hahaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HahatonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
